dd52cbcbbbdd7f407553c021b6884f695d3afd72
root <root@bastion.2cd7.example.opentlc.com>
2018-12-11 18:22:07 +0000

    Downgraded fluent-plugin-elasticsearch to 1.5 to accomodate Ruby <=2.0 requirement

--- common-install.sh
+++ common-install.sh
@@ -65,7 +65,7 @@ gem install -N --conservative --minimal-deps --no-document \
   'fluent-plugin-remote_syslog:<1.0.0' \
   fluent-plugin-splunk-ex \
   fluent-plugin-splunkhec \
-  fluent-plugin-elasticsearch
+  fluent-plugin-elasticsearch:1.5.0
 
 # set up directores so that group 0 can have access like specified in
 # https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html
